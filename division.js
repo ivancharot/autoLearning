8508ce3c142560938ee9cb07ae67a369b1b92764
Ivan Charot <ivancharot@gmail.com>
2022-05-26 10:29:55 +0300

    Conditional Statements and Data Types

---
+++ division.js
@@ -0,0 +1,4 @@
+console.log('string'/false);
+console.log('string'/256);
+console.log(512/true);
+console.log(512/false);
---
+++ multiplication.js
@@ -0,0 +1,4 @@
+console.log('string'*true);
+console.log('string'*1024);
+console.log(256*false);
+console.log(256*true);
